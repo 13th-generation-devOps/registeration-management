2e5f4c7a5691c1b23db6b3e2966fb317a63ba86e
LynaSovann <sovannlyna2004@gmail.com>
2025-09-15 07:49:59 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,11 @@ import { RegistrationForm } from "@/components/registration-form"
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-gradient-to-br from-background via-secondary/30 to-accent/10">
-      <div className="container mx-auto px-4 py-8">
-        <RegistrationForm />
-      </div>
-    </main>
+    <>Hello world</>
+    // <main className="min-h-screen bg-gradient-to-br from-background via-secondary/30 to-accent/10">
+    //   <div className="container mx-auto px-4 py-8">
+    //     <RegistrationForm />
+    //   </div>
+    // </main>
   )
 }
